77b48bdec778e1cd1c339d323bc85887c15178a7
Thalys Ravel <ravelthalys66@proton.me>
2025-12-31 14:56:16 -0300

    Added toString, HashCode and Equals

--- src/main/java/br/com/thalysravel/model/entity/Department.java
+++ src/main/java/br/com/thalysravel/model/entity/Department.java
@@ -37,7 +37,6 @@ public class Department implements Serializable {
         seller = new ArrayList<>();
     }
 
-
     public static long getSerialVersionUID() {
         return serialVersionUID;
     }
@@ -64,5 +63,35 @@ public class Department implements Serializable {
 
     public List<Seller> sellerList() {
         return seller;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Department other = (Department) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Department [id=" + id + ", name=" + name + ", seller=" + sellerList() + "]";
     }
 }
--- src/main/java/br/com/thalysravel/model/entity/Seller.java
+++ src/main/java/br/com/thalysravel/model/entity/Seller.java
@@ -80,4 +80,34 @@ public class Seller implements Serializable {
     public void setDepartment(Department department) {
         this.department = department;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seller other = (Seller) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", department=" + department.getName()+ "]";
+    }
 }
